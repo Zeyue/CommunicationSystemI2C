65eab29934b2d7cd979d8391a97f56eda05b1526
Zeyue Liang <zeyue.liang@student.ecp.fr>
2015-01-12 10:01:55 +0100

    SRC et SIM

---
+++ communication_system_i2c.srcs/sim_1/new/tb_module_RW.vhd
@@ -0,0 +1,102 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2015/01/06 14:07:35
+-- Design Name: 
+-- Module Name: tb_module_RW - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_module_RW is
+end tb_module_RW;
+
+architecture Behavioral of tb_module_RW is
+
+component module_RW is 
+ port (
+          DATA_IN : in STD_LOGIC_VECTOR (7 downto 0);
+          ACK_RECEIVED : in STD_LOGIC;
+          ACK_SENT : in STD_LOGIC;
+          CLK : in STD_LOGIC;
+                  
+          ADDR : out STD_LOGIC_VECTOR (6 downto 0);
+          NUM_IN : out INTEGER;
+          NUM_OUT : out INTEGER;
+          DATA_OUT : out STD_LOGIC_VECTOR (7 downto 0);
+          
+          WRITE : out STD_LOGIC; -- "1" start write
+          READ : out STD_LOGIC; -- "1" start read
+          
+          DATA_COMPUTED : out STD_LOGIC_VECTOR (2 downto 0));
+end component;
+
+signal DATA_IN : STD_LOGIC_VECTOR (7 downto 0) := "00000000";
+signal ACK_RECEIVED : STD_LOGIC := '0';
+signal ACK_SENT : STD_LOGIC := '0';
+signal CLK : STD_LOGIC := '0';
+signal ADDR :STD_LOGIC_VECTOR (6 downto 0) := "0000000";
+signal NUM_IN : INTEGER := 0;
+signal NUM_OUT : INTEGER := 0;
+signal DATA_OUT : STD_LOGIC_VECTOR (7 downto 0) := "00000000";
+signal WRITE : STD_LOGIC := '0';
+signal READ : STD_LOGIC := '0';
+signal DATA_COMPUTED : STD_LOGIC_VECTOR (2 downto 0) := "000";
+
+begin
+
+uut : module_RW port map(
+    DATA_IN => DATA_IN,
+    ACK_RECEIVED => ACK_RECEIVED,
+    ACK_SENT => ACK_SENT,
+    CLK => CLK,
+    ADDR => ADDR,
+    NUM_IN => NUM_IN,
+    NUM_OUT => NUM_OUT,
+    DATA_OUT => DATA_OUT,
+    WRITE => WRITE,
+    READ => READ,
+    DATA_COMPUTED => DATA_COMPUTED
+);
+
+CLK_SIM : process
+begin
+ CLK <= '1';
+ wait for 10ns;
+ CLK <= '0';
+ wait for 10ns;
+ end process;
+
+sim : process
+begin
+ ACK_RECEIVED <= '1';
+ ACK_SENT <= '0';
+ wait for 100ns;
+ ACK_RECEIVED <= '0';
+ ACK_SENT <= '1';
+ wait for 100ns;
+end process;
+end Behavioral;
---
+++ communication_system_i2c.srcs/sources_1/new/module_RW.vhd
@@ -0,0 +1,233 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2014/12/19 14:37:47
+-- Design Name: 
+-- Module Name: module_RW - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity module_RW is
+  Port ( 
+         DATA_IN : in STD_LOGIC_VECTOR (7 downto 0);
+         ACK_RECEIVED : in STD_LOGIC;
+         ACK_SENT : in STD_LOGIC;
+         CLK : in STD_LOGIC;
+                 
+         ADDR : out STD_LOGIC_VECTOR (6 downto 0);
+         NUM_IN : out INTEGER;
+         NUM_OUT : out INTEGER;
+         DATA_OUT : out STD_LOGIC_VECTOR (7 downto 0);
+         
+         WRITE : out STD_LOGIC; -- "1" start write
+         READ : out STD_LOGIC; -- "1" start read
+         
+         DATA_COMPUTED : out STD_LOGIC_VECTOR (2 downto 0));
+end module_RW;
+
+architecture Behavioral of module_RW is
+constant SLAVE_ADDR : STD_LOGIC_VECTOR (6 downto 0) := "1010011"; --0x53
+constant W : STD_LOGIC := '0';
+constant R : STD_LOGIC := '1';
+
+TYPE MODE is (PW_CTL, DATA_FORMAT_CTL, DATA);
+TYPE PW_MODE is (PW_REG_ADDR, MESURE);
+TYPE DATA_CTL_MODE is (DATA_CTL_REG_ADDR, VAL);
+TYPE DATA_MODE is (DATA_REG_ADDR, DATA_REC);
+TYPE XYZ_MODE is (X0, X1, Y0, Y1, Z0, Z1);
+
+signal internal_data_computed : STD_LOGIC_VECTOR (2 downto 0) := "000";
+signal currentMode : MODE;
+signal pw : PW_MODE;
+signal data_ctl : DATA_CTL_MODE;
+signal dataMode: DATA_MODE;
+signal xyz: XYZ_MODE;
+
+signal internalX0 : STD_LOGIC_VECTOR (7 downto 0):= "00000000";
+signal internalX1 : STD_LOGIC_VECTOR (7 downto 0):= "00000000";
+signal internalY0 : STD_LOGIC_VECTOR (7 downto 0):= "00000000";
+signal internalY1 : STD_LOGIC_VECTOR (7 downto 0):= "00000000";
+signal internalZ0 : STD_LOGIC_VECTOR (7 downto 0):= "00000000";
+signal internalZ1 : STD_LOGIC_VECTOR (7 downto 0):= "00000000";
+
+signal internalR : STD_LOGIC:= '0';
+signal internalW : STD_LOGIC:= '1';
+
+begin
+ADDR <= SLAVE_ADDR;
+NUM_IN <= 1;
+NUM_OUT <= 2;
+WRITE <= internalW;
+READ <= internalR;
+
+Transmit : process (CLK, ACK_RECEIVED, ACK_SENT) is 
+begin
+if CLK'EVENT and CLK = '1' then
+  case currentMode is
+  
+   when PW_CTL =>
+    case pw is
+     when PW_REG_ADDR =>
+      DATA_OUT <= "00101101"; -- power control register address 0x2D
+      if ACK_RECEIVED = '1' then
+       pw <= MESURE;
+      end if;
+     when MESURE =>
+      DATA_OUT <= "00111010"; -- mesure mode 0x3A
+      currentMode <= DATA_FORMAT_CTL;
+      data_ctl <= DATA_CTL_REG_ADDR;
+     end case;
+     
+   when DATA_FORMAT_CTL =>
+    case data_ctl is
+     when DATA_CTL_REG_ADDR =>
+      DATA_OUT <= "00110001"; -- data format control register address 0x31
+      if ACK_RECEIVED = '1' then
+       data_ctl <= VAL;
+      end if;
+     when VAL =>
+      DATA_OUT <= "00000001";
+      currentMode <= DATA;
+      dataMode <= DATA_REG_ADDR;
+      xyz <= X0;
+     end case;
+     
+   when DATA =>
+      case xyz is
+       when X0 =>
+        case dataMode is
+         when DATA_REG_ADDR =>
+          internalW <= '1';
+          internalR <= '0';
+          DATA_OUT <= "00110010"; -- 0x32
+          if ACK_RECEIVED = '1' then
+           dataMode <= DATA_REC;
+          end if;
+         when DATA_REC =>
+          internalW <= '0';
+          internalR <= '1';
+          if ACK_SENT = '1' then
+           internalX0 <= DATA_IN;
+           xyz <= X1;
+           dataMode <= DATA_REG_ADDR;
+          end if;
+         end case;
+       when X1 =>
+       case dataMode is
+        when DATA_REG_ADDR =>
+         internalW <= '1';
+         internalR <= '0';
+         DATA_OUT <= "00110011"; -- 0x33
+         if ACK_RECEIVED = '1' then
+          dataMode <= DATA_REC;
+         end if;
+        when DATA_REC =>
+         internalW <= '0';
+         internalR <= '1';
+         if ACK_SENT = '1' then
+          internalX1 <= DATA_IN;
+          xyz <= Y0;
+          dataMode <= DATA_REG_ADDR;
+         end if;
+        end case;
+       when Y0 =>
+       case dataMode is
+        when DATA_REG_ADDR =>
+         internalW <= '1';
+         internalR <= '0';
+         DATA_OUT <= "00110100"; -- 0x34
+         if ACK_RECEIVED = '1' then
+          dataMode <= DATA_REC;
+         end if;
+        when DATA_REC =>
+         internalW <= '0';
+         internalR <= '1';
+         if ACK_SENT = '1' then
+          internalY0 <= DATA_IN;
+          xyz <= Y1;
+          dataMode <= DATA_REG_ADDR;
+         end if;
+        end case;
+       when Y1 =>
+       case dataMode is
+        when DATA_REG_ADDR =>
+         internalW <= '1';
+         internalR <= '0';
+         DATA_OUT <= "00110101"; -- 0x35
+         if ACK_RECEIVED = '1' then
+          dataMode <= DATA_REC;
+         end if;
+        when DATA_REC =>
+         internalW <= '0';
+         internalR <= '1';
+         if ACK_SENT = '1' then
+          internalY1 <= DATA_IN;
+          xyz <= Z0;
+          dataMode <= DATA_REG_ADDR;
+         end if;
+        end case;
+       when Z0 =>
+       case dataMode is
+        when DATA_REG_ADDR =>
+         internalW <= '1';
+         internalR <= '0';
+         DATA_OUT <= "00110110"; -- 0x36
+         if ACK_RECEIVED = '1' then
+          dataMode <= DATA_REC;
+         end if;
+        when DATA_REC =>
+         internalW <= '0';
+         internalR <= '1';
+         if ACK_SENT = '1' then
+          internalZ0 <= DATA_IN;
+          xyz <= Z1;
+          dataMode <= DATA_REG_ADDR;
+         end if;
+        end case;
+       when Z1 =>
+       case dataMode is
+        when DATA_REG_ADDR =>
+         internalW <= '1';
+         internalR <= '0';
+         DATA_OUT <= "00110111"; -- 0x37
+         if ACK_RECEIVED = '1' then
+          dataMode <= DATA_REC;
+         end if;
+        when DATA_REC =>
+         internalW <= '0';
+         internalR <= '1';
+         if ACK_SENT = '1' then
+          internalZ1 <= DATA_IN;
+          xyz <= X0;
+          dataMode <= DATA_REG_ADDR;
+         end if;
+        end case;
+       end case;
+      end case;
+end if;
+end process;
+end Behavioral;
